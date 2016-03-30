78d56b015bdcaf51164c56bd4cfe404974267dd4
Neal Sheehan <nsheeha1@nd.edu>
2016-03-30 11:27:47 -0400

    Initial commit

---
+++ Find Me A MovieTests/Find_Me_A_MovieTests.swift
@@ -0,0 +1,36 @@
+//
+//  Find_Me_A_MovieTests.swift
+//  Find Me A MovieTests
+//
+//  Created by Neal Sheehan on 3/30/16.
+//  Copyright © 2016 Neal Sheehan. All rights reserved.
+//
+
+import XCTest
+@testable import Find_Me_A_Movie
+
+class Find_Me_A_MovieTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
