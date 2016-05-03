27fa3bd469f911aca7744b6d1138730dad058dfc
Neal Sheehan <nsheeha1@nd.edu>
2016-05-03 15:33:40 -0400

    Display helper text when no search results found

--- Find Me A Movie/SearchMoviesViewController.swift
+++ Find Me A Movie/SearchMoviesViewController.swift
@@ -12,10 +12,12 @@ class SearchMoviesViewController: UIViewController, UITableViewDelegate, UITable
     
     let tmdbService = TheMovieDatabaseService()
     var searchedMovies = [Movie]()
+    var search = false
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var searchTextField: UITextField!
     @IBAction func search(sender: UIButton) {
+        search = true
         if let searchTerm = searchTextField.text {
             searchedMovies.removeAll()
             tmdbService.searchMovies(searchTerm) {
@@ -34,7 +36,20 @@ class SearchMoviesViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        if searchedMovies.count > 0 || search == false{
+            tableView.tableHeaderView = nil
+            tableView.separatorStyle = .SingleLine
+            return 1
+        }
+        else {
+            let noResults = UILabel(frame: CGRectMake(0, 0, tableView.bounds.size.width, 100))
+            noResults.text = "No search results found"
+            noResults.textColor = UIColor.blackColor()
+            noResults.textAlignment = .Center
+            tableView.tableHeaderView = noResults
+            tableView.separatorStyle = .None
+            return 0
+        }
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
